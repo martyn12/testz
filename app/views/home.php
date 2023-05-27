4fe3094f54d2803666261c2cca9f7526986b9172
Alexander Martynov <martyn1201@gmail.com>
2023-05-27 11:02:58 +0400

    check for emty data in home.php

--- app/views/home.php
+++ app/views/home.php
@@ -1,15 +1,19 @@
 <section class="row">
     <div class="d-flex col-sm-12 justify-content-between">
-    <?php foreach($data as $row): ?>
-    <div class="mb-3 mx-auto mt-5">
-        <a href="main/product/<?= $row['id'] ?>" class="card" style="width: 18rem;">
-            <img src="/<?= $row['image'] ?>" class="card-img-top" alt="image" style="height: 250px; width: auto">
-            <div class="card-body">
-                <h5 class="card-title"><?= $row['title'] ?></h5>
-                <p class="card-text">Цена: <?= $row['price'] ?>$</p>
-            </div>
-        </a>
-    </div>
-    <?php endforeach ?>
+        <?php if ($data): ?>
+            <?php foreach ($data as $row): ?>
+                <div class="mb-3 mx-auto mt-5">
+                    <a href="main/product/<?= $row['id'] ?>" class="card" style="width: 18rem;">
+                        <img src="/<?= $row['image'] ?>" class="card-img-top" alt="image"
+                             style="height: 250px; width: auto">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $row['title'] ?></h5>
+                            <p class="card-text">Цена: <?= $row['price'] ?>$</p>
+                        </div>
+                    </a>
+                </div>
+            <?php endforeach ?>
+        <?php else: echo 'Не найдено товаров в вашей базе данных!' ?>
+        <?php endif ?>
     </div>
 </section>
